d15bd6d7e8475c8a6de8cb029a715cb67cfc760b
GvsKaushal <gvskaushal1@gmail.com>
2024-10-24 00:10:23 +0530

    1

--- src/ArrayProblems/RemoveDuplicates/SortedArray.java
+++ src/ArrayProblems/RemoveDuplicates/SortedArray.java
@@ -4,23 +4,23 @@ import java.util.Arrays;
 
 public class SortedArray {
     public static void main(String[] args) {
-        int[] arr={0,0,1,1,1,2,2,3,3,4};
-        int result=RemoveDuplicatesFromSortedArray(arr);
-        System.out.println("length = "+result+" array = "+ Arrays.toString(arr));
+        int[] arr = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+        int result = RemoveDuplicatesFromSortedArray(arr);
+        System.out.println("length = " + result + " array = " + Arrays.toString(arr));
     }
 
-    private static int RemoveDuplicatesFromSortedArray(int[] nums){
-        int len=nums.length;
+    private static int RemoveDuplicatesFromSortedArray(int[] nums) {
+        int len = nums.length;
 
-        if (len==0){
+        if (len == 0) {
             return 0;
         }
 
         int firstIndex = 1;
 
-        for (int secondIndex=1;secondIndex<len;secondIndex++){
-            if (nums[secondIndex] != nums[secondIndex-1]){
-                nums[firstIndex]=nums[secondIndex];
+        for (int secondIndex = 1; secondIndex < len; secondIndex++) {
+            if (nums[secondIndex] != nums[secondIndex - 1]) {
+                nums[firstIndex] = nums[secondIndex];
                 firstIndex++;
             }
         }
