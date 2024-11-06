b85f672fd85bd43cae9caf85d36824e2a3d7970f
GvsKaushal <gvskaushal1@gmail.com>
2024-11-06 11:00:25 +0530

    1

---
+++ src/BinarySearchProblems/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,39 @@
+package BinarySearchProblems;
+
+public class FindMinimumInRotatedSortedArray {
+
+    public static void main(String[] args) {
+
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+
+        int result = findMin(arr);
+        System.out.println(result);
+    }
+
+    private static int findMin(int[] nums) {
+
+        int n = nums.length;
+
+        int left = 0;
+        int right = n - 1;
+
+        int ans = Integer.MAX_VALUE;
+
+        while (left <= right) {
+
+            int midIndex = (left + right) / 2;
+
+//       Edge case (<=)
+            if (nums[left] <= nums[midIndex]) {
+                ans = Math.min(ans,nums[left]);
+                left = midIndex + 1;
+            } else {
+                ans = Math.min(ans,nums[midIndex]);
+                right = midIndex - 1;
+            }
+
+        }
+
+        return ans;
+    }
+}
---
+++ src/BinarySearchProblems/FindPeakElement.java
@@ -0,0 +1,39 @@
+package BinarySearchProblems;
+
+public class FindPeakElement {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 1, 3, 5, 6, 4};
+        int result = findPeakElement(arr);
+        System.out.println(result);
+    }
+
+    private static int findPeakElement(int[] nums) {
+
+        int n = nums.length;
+
+        if (n==1) return 0;
+
+        if (nums[0] > nums[1]) return 0;
+        if (nums[n - 1] > nums[n - 2]) return n - 1;
+
+        int left = 1;
+        int right = n - 2;
+
+        while (left <= right) {
+
+            int midIndex = (left + right) / 2;
+
+            if (nums[midIndex] > nums[midIndex - 1] && nums[midIndex] > nums[midIndex + 1]) return midIndex;
+
+            if (nums[midIndex - 1] < nums[midIndex] && nums[midIndex] < nums[midIndex + 1]) {
+                left = midIndex + 1;
+            } else {
+                right = midIndex - 1;
+            }
+        }
+
+        return -1;
+    }
+}
