4e10322457f22354f16e0f80910760eae9d1340a
GvsKaushal <gvskaushal1@gmail.com>
2024-11-07 15:42:00 +0530

    1

---
+++ src/BinarySearchProblems/FindTheSmallestDivisorGivenAThreshold.java
@@ -0,0 +1,52 @@
+package BinarySearchProblems;
+
+import java.util.Arrays;
+
+public class FindTheSmallestDivisorGivenAThreshold {
+
+    public static void main(String[] args) {
+
+        int[] nums = {21212, 10101, 12121};
+        int threshold = 1000000;
+        int result = smallestDivisor(nums, threshold);
+        System.out.println(result);
+    }
+
+    private static int smallestDivisor(int[] nums, int threshold) {
+
+        int left = 1;
+        int right = Arrays.stream(nums).max().getAsInt();
+
+        int Divisor = -1;
+
+        while (left <= right) {
+
+            int midIndex = (left + right) / 2;
+            int Th = getThreshold(nums, midIndex);
+
+            if (Th <= threshold) {
+                Divisor = midIndex;
+                right = midIndex - 1;
+            } else {
+                left = midIndex + 1;
+            }
+        }
+        return Divisor;
+    }
+
+    private static int getThreshold(int[] nums, int divisor) {
+
+        int Divisor = 0;
+
+        for (int n : nums) {
+
+            double divide = Math.ceil((double) n / (double) divisor);
+            Divisor = Divisor + (int) divide;
+        }
+
+        return Divisor;
+
+    }
+
+
+}
