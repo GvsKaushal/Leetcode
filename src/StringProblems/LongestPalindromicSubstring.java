e4710f385ffe4fb15194932183eddd9f559a9175
GvsKaushal <gvskaushal1@gmail.com>
2024-11-16 09:39:00 +0530

    1

---
+++ src/StringProblems/LongestPalindromicSubstring.java
@@ -0,0 +1,28 @@
+package StringProblems;
+
+public class LongestPalindromicSubstring {
+
+
+    public static void main(String[] args) {
+
+        String str = "babad";
+        String longestPalindrome = longestPalindrome(str);
+        System.out.println(longestPalindrome);
+    }
+
+    private static String longestPalindrome(String s) {
+
+        int start;
+        int len;
+
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+
+        }
+
+
+        return "";
+    }
+
+}
