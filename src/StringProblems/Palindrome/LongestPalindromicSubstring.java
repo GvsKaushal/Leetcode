136354648478338e9b6f48784d3520fd807db1ef
GvsKaushal <gvskaushal1@gmail.com>
2024-11-22 23:22:04 +0530

    1

--- src/StringProblems/Palindrome/LongestPalindromicSubstring.java
+++ src/StringProblems/Palindrome/LongestPalindromicSubstring.java
@@ -1,28 +1,58 @@
 package StringProblems.Palindrome;
 
+// https://www.youtube.com/watch?v=XYQecbcd6_c
+
 public class LongestPalindromicSubstring {
 
 
     public static void main(String[] args) {
 
-        String str = "babad";
+        String str = "cbbd";
         String longestPalindrome = longestPalindrome(str);
         System.out.println(longestPalindrome);
     }
 
     private static String longestPalindrome(String s) {
 
-        int start;
-        int len;
+        String longestPalindrome = "";
+
+        int left;
+        int right;
 
         int n = s.length();
 
+
         for (int i = 0; i < n; i++) {
+
+            // odd length
+            left = right = i;
+            while ((left >= 0 && right < n) && s.charAt(left) == s.charAt(right)) {
+
+                if ((right - left + 1) > longestPalindrome.length()) {
+                    longestPalindrome = s.substring(left, right + 1);
+                }
+
+                left--;
+                right++;
+            }
+
+            // even length
+            left = i;
+            right = i + 1;
+            while ((left >= 0 && right < n) && s.charAt(left) == s.charAt(right)) {
+
+                if ((right - left + 1) > longestPalindrome.length()) {
+                    longestPalindrome = s.substring(left, right + 1);
+                }
+
+                left--;
+                right++;
+            }
 
         }
 
 
-        return "";
+        return longestPalindrome;
     }
 
 }
