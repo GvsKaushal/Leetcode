9267da5d4afc50e4092d5953145c868eb8d82d04
GvsKaushal <gvskaushal1@gmail.com>
2024-11-27 23:03:53 +0530

    1

---
+++ src/StackProblems/AsteroidCollision.java
@@ -0,0 +1,57 @@
+package StackProblems;
+
+// https://www.youtube.com/watch?v=_eYGqw_VDR4
+// edge case check in code
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class AsteroidCollision {
+
+    public static void main(String[] args) {
+
+        int[] asteroids = {10, 2, -5};
+        int[] result = asteroidCollision(asteroids);
+        System.out.println(Arrays.toString(result));
+    }
+
+    private static int[] asteroidCollision(int[] asteroids) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        int len = asteroids.length;
+
+        int i = 0;
+        while (i < len) {
+            if (asteroids[i] > 0) {
+                stack.push(asteroids[i]);
+                i++;
+            } else {
+                int modValue = Math.abs(asteroids[i]);
+
+                while (!stack.isEmpty() && stack.peek() > 0 && modValue > stack.peek()) {
+                    stack.pop();
+                }
+                if (!stack.isEmpty() && stack.peek() > 0 && modValue == stack.peek()) {
+                    stack.pop();
+                    i++;
+                    continue;
+                }
+                // Edge case check
+                if (stack.isEmpty() || stack.peek() < 0) {
+                    stack.push(asteroids[i]);
+                }
+                i++;
+            }
+        }
+
+
+        int[] result = new int[stack.size()];
+
+        for (int j = result.length - 1; j >= 0; j--) {
+            result[j] = stack.pop();
+        }
+
+        return result;
+    }
+}
