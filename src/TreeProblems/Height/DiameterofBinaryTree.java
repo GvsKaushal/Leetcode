016075bb7d8991112eebd5af6d22bdf9391bf270
GvsKaushal <gvskaushal1@gmail.com>
2024-12-31 09:38:19 +0530

    1

---
+++ src/TreeProblems/Height/DiameterofBinaryTree.java
@@ -0,0 +1,28 @@
+package TreeProblems.Height;
+
+
+public class DiameterofBinaryTree {
+
+    // height of tree
+
+    public int diameterOfBinaryTree(TreeNode root) {
+
+        int[] Diameter = new int[1];
+
+        int result = heightOfBinaryTree(root, Diameter);
+
+        return Diameter[0];
+    }
+
+    public int heightOfBinaryTree(TreeNode root, int[] diameter) {
+
+        if (root == null) return 0;
+
+        int LH = heightOfBinaryTree(root.left, diameter);
+        int RH = heightOfBinaryTree(root.right, diameter);
+
+        diameter[0] = Math.max(diameter[0], LH + RH);
+
+        return 1 + Math.max(LH, RH);
+    }
+}
